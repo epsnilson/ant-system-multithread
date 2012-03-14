dc4d180296bf9a9e82c49a46b199ab6892f41c22
jackson.sildu <jackson.sildu@gmail.com>
2012-03-14 20:58:59 +0000

    Inclusão de termo de licensa GPL

--- AntSystem/src/br/com/ant/system/util/AntSystemUtil.java
+++ AntSystem/src/br/com/ant/system/util/AntSystemUtil.java
@@ -1,22 +1,36 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *   
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *   
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package br.com.ant.system.util;
 
 public class AntSystemUtil {
 
-		  private static AntSystemUtil instance;
+	private static AntSystemUtil	instance;
 
-		  public static synchronized AntSystemUtil getIntance() {
-					if (instance == null) {
-							  instance = new AntSystemUtil();
-					}
+	public static synchronized AntSystemUtil getIntance() {
+		if (instance == null) {
+			instance = new AntSystemUtil();
+		}
 
-					return instance;
-		  }
+		return instance;
+	}
 
-		  private AntSystemUtil() {
-		  }
+	private AntSystemUtil() {
+	}
 
-		  public int getAleatorio(int min, int max) {
-					int aleatorio = min + (int) ((max - min) * Math.random());
-					return aleatorio;
-		  }
+	public int getAleatorio(int min, int max) {
+		int aleatorio = min + (int) ((max - min) * Math.random());
+		return aleatorio;
+	}
 }
